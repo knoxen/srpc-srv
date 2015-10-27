2c81c8ebc969be74197229c28d1c43da69926075
dingo sky <paul@dingosky.com>
2015-10-26 18:27:18 -0700

    Refactor

--- src/srpc_http.erl
+++ src/srpc_http.erl
@@ -10,15 +10,14 @@
         ,user_key_validate/2
         ,encrypt_data/2
         ,decrypt_packet/2 
-        ,epoch/2
+        ,server_epoch/2
         ]).
 
 -define(APP_NAME, srpc_http).
 
--define(HTTP_VSN,          1).
--define(HTTP_VSN_BITS,     8).
--define(EPOCH_BITS,       32).
--define(EPOCH_STAMP_SIZE, 16).
+-define(SRPC_HTTP_VSN,       1).
+-define(SRPC_HTTP_VSN_BITS,  8).
+-define(EPOCH_BITS,         32).
 
 %%====================================================================
 %% API functions
@@ -45,8 +44,8 @@ lib_key_validate(LibKeyId, ValidationRequest) ->
       case srpc_lib:lib_key_process_validation_request(SrpData, ValidationRequest) of
         {ok, {LibKeyInfo, ClientChallenge, ReqData}} ->
           RespData = srpc_api_impl:lib_key_validation_data(ReqData),
-          Epoch = erlang:system_time(seconds),
-          SrpcHttpRespData = <<Epoch:?EPOCH_BITS, RespData/binary>>,
+          DataEpoch = erlang:system_time(seconds),
+          SrpcHttpRespData = <<DataEpoch:?EPOCH_BITS, RespData/binary>>,
           case srpc_lib:lib_key_create_validation_response(SrpData, LibKeyInfo,
                                                            ClientChallenge, SrpcHttpRespData) of
             {ok, RespPacket} ->
@@ -177,16 +176,16 @@ user_key_validate(LibKeyId, ValidationRequest) ->
       Error
   end.
 
-epoch(LibKeyId, Body) ->
+server_epoch(LibKeyId, Body) ->
   case lib_key_data_for_id(LibKeyId) of
     {ok, LibKeyInfo} ->
       case srpc_encryptor:decrypt(LibKeyInfo, Body) of
-        {ok, <<RandomStamp:?EPOCH_STAMP_SIZE/binary>>} ->
-          Epoch = erlang:system_time(seconds),
-          RespData = <<Epoch:?EPOCH_BITS, RandomStamp/binary>>,
+        {ok, <<RandomStamp/binary>>} ->
+          DataEpoch = erlang:system_time(seconds),
+          RespData = <<DataEpoch:?EPOCH_BITS, RandomStamp/binary>>,
           srpc_encryptor:encrypt(LibKeyInfo, RespData);
         {ok, _ReqData} ->
-          {error, <<"Invalid Epoch stamp">>};
+          {error, <<"Invalid data epoch stamp">>};
         Error ->
           Error
       end;
@@ -201,6 +200,7 @@ encrypt_data(KeyInfo, RespData) ->
 decrypt_packet(KeyInfo, Packet) ->
   case srpc_lib:decrypt(KeyInfo, Packet) of
     {ok, SrpcHttpReqData} ->
+      io:format("~p Decrypt SrpcHttpReqData~n~p~n", [?MODULE, SrpcHttpReqData]),
       parse_http_req_data(SrpcHttpReqData);
     Error ->
       Error
@@ -244,21 +244,21 @@ rand_session_id() ->
     end,
   {SidLen, srpc_util:rand_id(SidLen)}.
 
-
-parse_http_req_data(<<?HTTP_VSN:?HTTP_VSN_BITS, DataEpoch:?EPOCH_BITS, ReqData/binary>>) ->
+parse_http_req_data(<<?SRPC_HTTP_VSN:?SRPC_HTTP_VSN_BITS, 
+                      DataEpoch:?EPOCH_BITS, ReqData/binary>>) ->
   Tolerance = req_age_tolerance(),
-  Epoch = erlang:system_time(seconds),
-  case (Epoch - DataEpoch) < Tolerance of
+  ReqEpoch = erlang:system_time(seconds),
+  case (ReqEpoch - DataEpoch) < Tolerance of
     true ->
       {ok, ReqData};
     false ->
       {error, <<"Request data age is greater than tolerance">>}
   end;
-parse_http_req_data(<<_:?HTTP_VSN_BITS, _Rest/binary>>) ->
+parse_http_req_data(<<_:?SRPC_HTTP_VSN_BITS, _Rest/binary>>) ->
   {error, <<"Invalid Srpc Http version number">>};
 parse_http_req_data(_HttpReqData) ->
   {error, <<"Invalid HTTP request data">>}.
 
 create_http_resp_data(SrpcHttpData, RespData) ->
-  Epoch = erlang:system_time(seconds),
-  << ?HTTP_VSN:8, Epoch:?EPOCH_BITS, SrpcHttpData/binary, RespData/binary >>.
+  RespEpoch = erlang:system_time(seconds),
+  << ?SRPC_HTTP_VSN:8, RespEpoch:?EPOCH_BITS, SrpcHttpData/binary, RespData/binary >>.

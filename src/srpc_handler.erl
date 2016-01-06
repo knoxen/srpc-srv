aa64b9470d631cfe14380665e17123fb16fb9544
dingo sky <paul@dingosky.com>
2016-01-06 09:02:44 -0800

    Add srpc handler behavior

---
+++ src/srpc_handler.erl
@@ -0,0 +1,27 @@
+-module(srpc_handler).
+
+-author("paul@knoxen.com").
+
+-callback put(Id :: binary(), Value :: term(), Cache :: atom) ->
+  ok | {error, Reason :: string()}.
+
+-callback get(Id :: binary(), Cache :: atom) ->
+  {ok, Value :: term()} | undefined.
+
+-callback delete(Id :: binary(), Cache :: atom) ->
+  {ok, Value :: term()} | undefined.
+
+-callback lib_key_exchange_data(ReqData :: binary()) ->
+  RespData :: binary().
+
+-callback lib_key_validation_data(ReqData :: binary()) ->
+  RespData :: binary().
+
+-callback registration_data(UserId :: binary(), ReqData :: binary()) ->
+  RespData :: binary().
+
+-callback user_key_exchange_data(ReqData :: binary()) ->
+  RespData :: binary().
+
+-callback user_key_validation_data(ReqData :: binary()) ->
+  RespData :: binary().

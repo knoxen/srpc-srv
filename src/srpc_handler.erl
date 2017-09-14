1f43b807ba9daceffd172b4d87ef15d3af909ac6
dingo sky <paul@dingosky.com>
2017-09-14 13:39:39 -0700

    Optional behavior functions documentation

--- src/srpc_handler.erl
+++ src/srpc_handler.erl
@@ -2,35 +2,65 @@
 
 -author("paul@knoxen.com").
 
+-type storage() :: exchange | key | registration
+
 %%================================================================================================
-%%
-%% @doc SRPC handler function
+%% @doc SRPC handler functions
 %%
 %%================================================================================================
 %%------------------------------------------------------------------------------------------------
-%% @doc Put Value for Key in storage Type
+%% @doc Put <code>Value</code> for <code>Key</code> under storage <code>Type</code>
 %%
+%% Returns <code>ok</code> or <code>{error, Reason}</code>
 %%------------------------------------------------------------------------------------------------
--callback put(Key :: binary(), Value :: term(), 
-              Type :: exchange | key | registration) ->
+-callback put(Key :: binary(), Value :: term(), Type :: storage()) ->
   ok | {error, Reason :: string()}.
 
 %%------------------------------------------------------------------------------------------------
-%% @doc Get value for Key in storage Type
+%% @doc Get value for <code>Key</code> in storage <code>Type</code>
 %%
+%% Return the <code>value</code> stored or <code>undefined</code>
 %%------------------------------------------------------------------------------------------------
--callback get(Key :: binary(), Type :: exchange | key | registration) ->
+-callback get(Key :: binary(), Type :: storage) ->
   {ok, Value :: term()} | undefined.
 
 %%------------------------------------------------------------------------------------------------
-%% @doc Delete value for Key in storage Type
+%% @doc Delete value for <code>Key</code> in storage <code>Type</code>
 %%
-%% Note Type does not include registration, which should be managed through an existing,
-%% valid client and hence should be handled via application API calls.
+%% <code>Type</code> does not include <code>registration</code>, which should be managed
+%%  through an existing, valid client and hence should be handled via application API calls.
 %%
+%% Returns <code>ok</code> or <code>{error, Reason}</code>
 %%------------------------------------------------------------------------------------------------
 -callback delete(Key :: binary(), Type :: exchange | key) ->
   ok | {error, Reason :: string()}.
 
-
 %% CxTBD Optional functions
+%%
+%% client_id_bits/0
+%%   - Number of entropy bits for client IDs
+%%
+%% client_id_charset/0
+%%   - Charset for client IDs
+%%
+%% req_age_tolerance/0
+%%   - Max time in seconds to consider a request as valid
+%%
+%% nonce/1
+%%   - Handle request nonce
+%%
+%% lib_exchange_data/1
+%%   - Handle application specific client data included in lib key exchange message
+%%
+%% lib_confirm_data/1
+%%   - Handle application specific client data included in lib key confirm message
+%%
+%% registration_data/1
+%%   - Handle application specific client data included in registration message
+%% 
+%% user_exchange_data/1
+%%   - Handle application specific client data included in user key exchange message
+%%
+%% user_confirm_data/1
+%%   - Handle application specific client data included in user key confirm message
+%%
